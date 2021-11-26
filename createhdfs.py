385230fbe280588cd2391319a922ed78f77490c2
Vinay Naidu <vinaynaidu@Vinays-MacBook-Pro.local>
2021-11-26 20:08:55 +0530

    Changed hardcoded paths to absolute paths using config.json

--- createhdfs.py
+++ createhdfs.py
@@ -2,27 +2,46 @@ import json
 import os
 from shutil import rmtree
 
-f = open("config_sample.json")
-
-data = json.load(f)
-
 BLOCK_SIZE = 64
 REPLICATION_FACTOR = 3
 NUM_DATANODES = 5
 DATANODE_SIZE = 10
 SYNC_PERIOD = 180
 
+f = open("config_sample.json")
+config = json.load(f)
+block_size = config['block_size']
+path_to_datanodes = config['path_to_datanodes']
+path_to_namenodes = config['path_to_namenodes']
+replication_factor = config['replication_factor']
+num_datanodes = config['num_datanodes']
+datanode_size = config['datanode_size']
+sync_period = config['sync_period']
+datanode_log_path = config['datanode_log_path']
+namenode_log_path = config['namenode_log_path']
+namenode_checkpoints = config['namenode_checkpoints']
+fs_path = config['fs_path']
+dfs_setup_config = config['dfs_setup_config']
+
+setupfiledir = config['dfs_setup_config'][:-10]
 try:
-    rmtree('DATANODE')
+    os.mkdir(setupfiledir)
+except:
+    pass
+setupfile = open(dfs_setup_config, 'w')
+setupfile.write(str(json.dumps(config)))
+
+try:
+    rmtree(path_to_datanodes)
 except:
     pass
 try:
-    rmtree('NAMENODE')
+    rmtree(path_to_namenodes)
 except:
     pass
 
-os.mkdir('DATANODE')
-os.mkdir('NAMENODE')
+os.mkdir(path_to_datanodes)
+os.mkdir(path_to_namenodes)
 
 datanodestring = '''import socket
 import time
@@ -36,16 +55,76 @@ def datanode{}HB():
     while True:
         UDPClientSocket.sendto(bytesToSend, serverAddressPort)'''
 
-for i in range(1, NUM_DATANODES+1):
-    dirname = './DATANODE/datanode{}/'.format(i)
+namenodestring = '''import socket
+import time
+import datetime
+import json
+
+f = open("/Users/vinaynaidu/DFS/setup.json")
+config = json.load(f)
+block_size = config['block_size']
+path_to_datanodes = config['path_to_datanodes']
+path_to_namenodes = config['path_to_namenodes']
+replication_factor = config['replication_factor']
+num_datanodes = config['num_datanodes']
+datanode_size = config['datanode_size']
+sync_period = config['sync_period']
+datanode_log_path = config['datanode_log_path']
+namenode_log_path = config['namenode_log_path']
+namenode_checkpoints = config['namenode_checkpoints']
+fs_path = config['fs_path']
+dfs_setup_config = config['dfs_setup_config']
+
+HEARBEAT_TIMEPERIOD = 5.0
+
+masterset = set(range(1, num_datanodes + 1))
+
+def namenodereceiveheartbeat1():
+	localIP = "127.0.0.1"
+	localPort = 2000
+	bufferSize = 1024
+	
+	msgFromServer = "Hello UDP Client"
+	bytesToSend = str.encode(msgFromServer)
+	UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+	UDPServerSocket.bind((localIP, localPort))
+	print("NAMENODE server up and listening")
+
+	set1 = set()
+
+	while(True):
+		start = time.time()
+		while(time.time() < start + HEARBEAT_TIMEPERIOD):
+			bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
+			message = int(bytesAddressPair[0].decode())
+			set1.add(int(message))
+		if len(set1) == num_datanodes:
+			print("200, All datanodes functioning", datetime.datetime.fromtimestamp(time.time()))
+			set1 = set()
+		else:
+			faultydatanodes = masterset - set1
+			print("404, Some datanodes didn't send heartbeat", faultydatanodes)
+			set1 = set()
+			#should write code to remove the datanode from the metadata file
+			#todo'''
+			
+try:
+    os.mkdir(path_to_namenodes)
+except:
+    pass
+filename = path_to_namenodes + 'namenode.py'
+handle = open(filename, 'w')
+handle.write(namenodestring)
+
+for i in range(1, num_datanodes + 1):
+    dirname = path_to_datanodes + 'datanode{}/'.format(i)
     os.mkdir(dirname)
-    filename = 'DATANODE/datanode{}/datanode{}.py'.format(i, i)
-    open('DATANODE/datanode{}/__init__.py'.format(i, i), 'w').close()
+    filename = path_to_datanodes + 'datanode{}'.format(i) + '/datanode{}.py'.format(i)
     open(filename, 'w').close()
     filehandle = open(filename,"w")
     filehandle.write(datanodestring.format(i, i))
     filehandle.close()
-    open('__init__.py', 'w').close()
-    for j in range(1, DATANODE_SIZE+1):
+    for j in range(1, datanode_size + 1):
         filename = 'DATANODE/datanode{}/block{}.txt'.format(i, j)
+        filename = dirname + 'block{}.txt'.format(j)
         open(filename, 'w').close()
--- script.py
+++ script.py
@@ -1,23 +1,40 @@
 import threading
 from threading import Thread
 from multiprocessing import Process
-from namenoderecieveheartbeat import namenodereceiveheartbeat1
+import json
+import sys
 
-BLOCK_SIZE = 64
-REPLICATION_FACTOR = 3
-NUM_DATANODES = 5
-DATANODE_SIZE = 10
-SYNC_PERIOD = 180
+#change path to this file accordingly
+f = open("/Users/vinaynaidu/DFS/setup.json")
+config = json.load(f)
+block_size = config['block_size']
+path_to_datanodes = config['path_to_datanodes']
+path_to_namenodes = config['path_to_namenodes']
+replication_factor = config['replication_factor']
+num_datanodes = config['num_datanodes']
+datanode_size = config['datanode_size']
+sync_period = config['sync_period']
+datanode_log_path = config['datanode_log_path']
+namenode_log_path = config['namenode_log_path']
+namenode_checkpoints = config['namenode_checkpoints']
+fs_path = config['fs_path']
+dfs_setup_config = config['dfs_setup_config']
+
+
+sys.path.append(path_to_datanodes)
+sys.path.append(path_to_namenodes)
+from namenode import namenodereceiveheartbeat1
 
 dsthreads = {}
 
-for i in range(1, NUM_DATANODES + 1):
-    exec("from DATANODE.datanode{}.datanode{} import datanode{}HB".format(i, i, i))
+for i in range(1, num_datanodes + 1):
+    sys.path.append(path_to_datanodes + 'datanode{}/'.format(i))
+    exec("from datanode{} import datanode{}HB".format(i, i))
     exec("dsthreads['datanodehbthread{}'] = threading.Thread(target = datanode{}HB, name = 'DatanodeHBThread{}')".format(i, i, i))
 
 
 namenodeHBthread = threading.Thread(target=namenodereceiveheartbeat1, name='namenodeHBthread')
 namenodeHBthread.start()
 
-for i in range(1, NUM_DATANODES + 1):
+for i in range(1, num_datanodes + 1):
     dsthreads['datanodehbthread{}'.format(i)].start()
